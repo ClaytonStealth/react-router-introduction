341cb8b325e922dbc90d343c4cb30e3050722208
claytonstealth <claytonses@yahoo.com>
2022-10-19 16:26:09 -0400

    stretches complete

--- src/Components/SingupCard.js
+++ src/Components/SingupCard.js
@@ -1,5 +1,18 @@
+import userEvent from "@testing-library/user-event";
+import { useOutletContext, useParams } from "react-router-dom";
 const SignupCard = () => {
-  return <div></div>;
+  const params = useParams();
+  const [signupList] = useOutletContext();
+  const foundSignup = signupList.find((signup) => {
+    return signup.email === params.email;
+  });
+  return (
+    <div>
+      {foundSignup && <p>{foundSignup.firstName}</p>}
+      {foundSignup && <p>{foundSignup.lastName}</p>}
+      {foundSignup && <p>{foundSignup.email}</p>}
+    </div>
+  );
 };
 
 export default SignupCard;
--- src/Pages/Signups.js
+++ src/Pages/Signups.js
@@ -1,16 +1,21 @@
-import { useOutletContext, useNavigate } from "react-router-dom";
+import { useOutletContext, useNavigate, Outlet } from "react-router-dom";
 const Signups = () => {
   const navigate = useNavigate();
   const [signupList] = useOutletContext();
   return (
     <div>
       <h1>Sign Up</h1>
-      <select>
+      <select
+        onChange={(e) => {
+          navigate(`/signups/${e.target.value}`);
+        }}
+      >
         {signupList.map((signup) => {
           return <option value={signup.email}>{signup.firstName}</option>;
         })}
         <option></option>
       </select>
+      <Outlet context={[signupList]} />
     </div>
   );
 };
